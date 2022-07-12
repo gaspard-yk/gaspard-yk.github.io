b08abdb3626c0de4f89e199d3f5f3d7d1ed61b25
gaspard-yk <101567471+gaspard-yk@users.noreply.github.com>
2022-07-12 16:09:16 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,9 @@
+ const { Telegraf, Markup } = require('telegraf');
+ const keyboard = Markup.inlineKeyboard([
+   Markup.button.webApp('❤️', 'https://gaspard-yk.github.io'),
+ ]);
 
- const { Telegraf } = require('telegraf');
  const bot = new Telegraf('5444045893:AAEFNihtjBIK90frln1y3JPDRZvp1BehKwY'); //сюда помещается токен, который дал botFather
- bot.start((ctx) => ctx.reply('Welcome')); //ответ бота на команду /start
- bot.help((ctx) => ctx.reply('Send me a sticker')); //ответ бота на команду /help
- bot.on('sticker', (ctx) => ctx.reply('')); //bot.on это обработчик введенного юзером сообщения, в данном случае он отслеживает стикер, можно использовать обработчик текста или голосового сообщения
+ bot.on('message', (ctx) => ctx.reply('my test', keyboard))
  bot.hears('hi', (ctx) => ctx.reply('Hey there'));// bot.hears это обработчик конкретного текста, данном случае это - "hi"
  bot.launch(); // запуск бота
