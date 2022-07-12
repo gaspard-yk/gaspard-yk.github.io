f9af75a49428641aaf6c0e2cde24ce5e1d12bf64
gaspard-yk <101567471+gaspard-yk@users.noreply.github.com>
2022-07-12 16:23:57 +0500

    Update unit.js

--- unit.js
+++ unit.js
@@ -97,14 +97,6 @@ const buyFunction=cart=>{
       return;
    }
    else{
-     let sumprice=0;
-     let cartstring="";
-     for(let id in cart){
-      sumprice = sumprice+cart[id]['count']*cart[id]['cost'];
-      cartstring=cartstring+"\n name "+ cart[id]['name']+" count "+cart[id]['count']+" cost "+cart[id]['cost'];
-
-     }
-    alert("sum = "+sumprice + "\n"+ cartstring);
     tg.sendData("it works"); 
 
    }
