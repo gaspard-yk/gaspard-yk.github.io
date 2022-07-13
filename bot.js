c1ad06f29fa6b611bb3d8266483b9b47a6e70702
gaspard-yk <101567471+gaspard-yk@users.noreply.github.com>
2022-07-13 10:48:09 +0500

    x

--- bot.js
+++ bot.js
@@ -2,9 +2,12 @@
  const keyboard = Markup.inlineKeyboard([
    Markup.button.webApp('test web app', 'https://gaspard-yk.github.io'),
  ]);
+ const urlbutton = Markup.keyboard([
+  Markup.button.webApp('test web app', 'https://gaspard-yk.github.io')
+ ])
 
  const bot = new Telegraf('5444045893:AAEFNihtjBIK90frln1y3JPDRZvp1BehKwY'); //сюда помещается токен, который дал botFather
  bot.hears('hi', (ctx) => ctx.reply('Hey there'));// bot.hears это обработчик конкретного текста, данном случае это - "hi"
- bot.on('message', (ctx) => ctx.reply('my test', keyboard))
- bot.on('web_app_data',(ctx)=>ctx.reply(ctx.webAppData.data))
+ bot.on('message', (ctx) => ctx.reply('my test',urlbutton));
+ bot.on('web_app_data',(ctx)=>ctx.reply(ctx.webAppData.data));
  bot.launch(); // запуск бота
