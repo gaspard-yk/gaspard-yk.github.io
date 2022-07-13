00258f9a0531a9b4f68273e43a7a8bd0105ef052
gaspard-yk <101567471+gaspard-yk@users.noreply.github.com>
2022-07-13 10:16:47 +0500

    y

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
  const { Telegraf, Markup } = require('telegraf');
  const keyboard = Markup.inlineKeyboard([
-   Markup.button.webApp('❤️', 'https://gaspard-yk.github.io'),
+   Markup.button.webApp('test web app', 'https://gaspard-yk.github.io'),
  ]);
 
  const bot = new Telegraf('5444045893:AAEFNihtjBIK90frln1y3JPDRZvp1BehKwY'); //сюда помещается токен, который дал botFather
