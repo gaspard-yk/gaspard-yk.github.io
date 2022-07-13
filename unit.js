89b50e2758602a1fbe0a670ded0977de3975d1a8
gaspard-yk <101567471+gaspard-yk@users.noreply.github.com>
2022-07-13 11:00:16 +0500

    1

--- unit.js
+++ unit.js
@@ -11,16 +11,16 @@ Telegram.WebApp.onEvent('mainButtonClicked', function(){
 });
 
 
-let usercard = document.getElementById("usercard"); //получаем блок usercard 
-let profName = document.createElement('p'); //создаем параграф
-profName.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}
-${tg.initDataUnsafe.user.username} (${tg.initDataUnsafe.user.language_code})`;
+//let usercard = document.getElementById("usercard"); //получаем блок usercard 
+//let profName = document.createElement('p'); //создаем параграф
+//profName.innerText = `${tg.initDataUnsafe.user.first_name}
+//${tg.initDataUnsafe.user.last_name}
+//${tg.initDataUnsafe.user.username} (${tg.initDataUnsafe.user.language_code})`;
 //выдем имя, "фамилию", через тире username и код языка
-usercard.appendChild(profName); //добавляем 
-let userid = document.createElement('p'); //создаем еще параграф 
-userid.innerText = `${tg.initDataUnsafe.user.id}`; //показываем user_id
-usercard.appendChild(userid); //добавляем 
+//usercard.appendChild(profName); //добавляем 
+//let userid = document.createElement('p'); //создаем еще параграф 
+//userid.innerText = `${tg.initDataUnsafe.user.id}`; //показываем user_id
+//usercard.appendChild(userid); //добавляем 
 
 
 let range ={
